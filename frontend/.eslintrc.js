5f1de21903c36fb660594e16cf350198fa612b8e
chandra-pangea <santprakash28877@gmail.com>
2025-09-27 01:47:44 +0530

    ts update

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,23 +3,26 @@ module.exports = {
     root: true,
     parser: '@typescript-eslint/parser', // TypeScript parser
     parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
+      ecmaVersion: 2021, // Modern JS syntax
+      sourceType: 'module', // Enable imports
+      ecmaFeatures: {
+        jsx: true, // Enable JSX parsing
+      },
     },
     plugins: [
-      '@typescript-eslint', // TypeScript rules
-      'react',              // React specific rules
+      '@typescript-eslint', // TypeScript-specific rules
+      'react',              // React rules
       'react-hooks',        // React hooks rules
       'prettier',           // Prettier integration
     ],
     extends: [
       'react-app',
       'react-app/jest',
-      'eslint:recommended',               // Recommended JS rules
+      'eslint:recommended',                  // Recommended JS rules
       'plugin:@typescript-eslint/recommended', // Recommended TS rules
-      'plugin:react/recommended',         // Recommended React rules
-      'plugin:react-hooks/recommended',   // Recommended React hooks rules
-      'plugin:prettier/recommended',      // Prettier integration
+      'plugin:react/recommended',            // Recommended React rules
+      'plugin:react-hooks/recommended',      // Recommended React hooks rules
+      'plugin:prettier/recommended',         // Prettier integration
     ],
     rules: {
       /** ----------------------
@@ -27,32 +30,32 @@ module.exports = {
        * ---------------------- */
       'react/prop-types': 'off',          // TypeScript handles props
       'react/react-in-jsx-scope': 'off',  // Not needed in React 17+
-      'react/jsx-uses-vars': 'error',
+      'react/jsx-uses-vars': 'error',     // Prevent unused JSX variables
   
       /** ----------------------
        * React Hooks Rules
        * ---------------------- */
       'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/exhaustive-deps': 'warn', // Warn about missing deps
   
       /** ----------------------
        * TypeScript Rules
        * ---------------------- */
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // Allow _ prefix
+      '@typescript-eslint/explicit-function-return-type': 'off', // Let TS infer
   
       /** ----------------------
        * General JS Rules
        * ---------------------- */
       'no-console': ['error', { allow: ['warn', 'error'] }], // Blocks console.log
-      'no-debugger': 'error',
-      eqeqeq: ['error', 'always'],
-      curly: ['error', 'all'],
+      'no-debugger': 'error',                                 // Prevent debugger
+      eqeqeq: ['error', 'always'],                            // Require ===
+      curly: ['error', 'all'],                                // Always use curly braces
   
       /** ----------------------
-       * Prettier formatting
+       * Prettier Formatting
        * ---------------------- */
-      'prettier/prettier': ['error'],
+      'prettier/prettier': ['error'], // Make prettier issues show as ESLint errors
     },
     settings: {
       react: {
@@ -65,5 +68,17 @@ module.exports = {
       node: true,
       jest: true,
     },
+    overrides: [
+      {
+        files: ['*.ts', '*.tsx'], // Apply TypeScript rules only to TS files
+        rules: {
+          '@typescript-eslint/explicit-module-boundary-types': 'off',
+        },
+      },
+      {
+        files: ['*.js', '*.jsx'], // JS-specific overrides
+        rules: {},
+      },
+    ],
   };
   
